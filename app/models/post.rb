b6c7c9c144ec0a8633bb3b20f6d43cc6512ee359
A. Demko <artdemko@gmail.com>
2013-11-13 10:29:45 +0100

    Search change

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,7 @@ class Post < ActiveRecord::Base
 
   def self.search(search)
   	if search
-   	  find(:all, :conditions => ['content LIKE ?', "%#{search}%"])
+   	  where("content LIKE ?", "%#{search}%")
  	else
    	  find(:all)
   	end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
 
   def self.search(search)
   if search
-    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+  	where('name LIKE ?', "%#{search}%")
   else
     find(:all)
   end
