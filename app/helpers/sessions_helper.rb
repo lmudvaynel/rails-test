9d8bf354b367b71c295416f9b96897b1dc1ae52e
A. Demko <artdemko@gmail.com>
2014-01-13 12:12:21 +0300

    Commit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,7 +26,7 @@ module SessionsHelper
 	end
 
 	def signed_in_user
-  	redirect_to signin_url, notice: "Please sign in." unless signed_in?
+		redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
 
 end
